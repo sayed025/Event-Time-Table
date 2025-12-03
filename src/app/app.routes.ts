7b3806035bab9d5dec551c170d1cf0b580d22dc7
sayed025 <sayed.cse18@gmail.com>
2025-12-03 12:37:36 +0600

    create event time table component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { EventTimeTable } from './features/event-time-table/event-time-table';
+  
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+  path: '',
+  loadComponent: () => import('./features/event-time-table/event-time-table').then(m => m.EventTimeTable)
+}
+];
---
+++ src/app/features/event-time-table/event-time-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-time-table',
+  imports: [],
+  templateUrl: './event-time-table.html',
+  styleUrl: './event-time-table.css',
+})
+export class EventTimeTable {
+
+}
